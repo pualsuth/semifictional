870109e3f4e6d6a7fa881fd10641935237f6c3fc
pual <78347620+pualsuth@users.noreply.github.com>
2024-03-05 11:58:40 +0800

    camera and render

--- js/extra.js
+++ js/extra.js
@@ -5,7 +5,8 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 // const controls = new OrbitControls( camera, renderer.domElement );
 const loader = new GLTFLoader();
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 25, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.set(1, 1, 20);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -18,3 +19,9 @@ loader.load( 'js/coffee_cup.gltf', function (gltf) {
     scene.add(coffee);
 
 });
+
+function render() {
+    renderer.render(scene, camera);
+};
+
+render();
