e75ce970a64419aa7ef29d788952109859633c61
pual <78347620+pualsuth@users.noreply.github.com>
2024-03-05 11:44:54 +0800

    add scene

--- js/extra.js
+++ js/extra.js
@@ -4,6 +4,12 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 // const controls = new OrbitControls( camera, renderer.domElement );
 const loader = new GLTFLoader();
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
 loader.load( 'js/coffee_cup.gltf', function (gltf) {
     var coffee = gltf.scene;
