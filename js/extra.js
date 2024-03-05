af3578c56f64d79c19f3171afaae8d0e22132219
pual <78347620+pualsuth@users.noreply.github.com>
2024-03-05 11:25:36 +0800

    added coffee cup

--- js/extra.js
+++ js/extra.js
@@ -4,3 +4,11 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const controls = new OrbitControls( camera, renderer.domElement );
 const loader = new GLTFLoader();
+
+loader.load( '/coffee_cup.gltf', function (gltf) {
+    coffee = gltf.scene;
+    coffee.scale.set(4, 4, 4);
+    coffee.position.y = 4;
+    scene.add(coffee);
+
+})
